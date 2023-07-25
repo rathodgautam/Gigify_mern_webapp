95a5f1638d641dae4d84aa1e8d5aae9d1f1aa129
Gautam Rathod <rathodgautam7717@gmail.com>
2023-07-25 19:18:28 +0530

    changes

--- api/server.js
+++ api/server.js
@@ -41,7 +41,7 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "../dist"))); 
   app.get("*", (req, res) =>
-   res.sendFile(path.resolve(__dirname1,  "../dist", "index.html"))
+   res.sendFile(path.resolve(__dirname1,  "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
